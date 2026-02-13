fd5519d25e51c201be5041ea6a82ad440f2c0848
bhppham <252704436go@gmail.com>
2026-02-13 13:42:41 -0500

    Added HTMLNode and TextNode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -25,3 +25,30 @@ class TestHTMLNode(unittest.TestCase):
             "p", "some text", props={"onClick": '()=>{alert("hello");}'}
         )
         self.assertEqual('onClick="()=>{alert("hello");}"', someNode.props_to_html())
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_node(self):
+        paragraph = LeafNode(
+            "p", "some text", {"class": "color-white", "background-color": "#000"}
+        )
+        self.assertEqual(
+            '<p class="color-white" background-color="#000">some text</p>',
+            paragraph.to_html(),
+        )
+
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
